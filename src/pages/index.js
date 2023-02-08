37d1992d9820e5d9cede00c67a0948588440ef9f
alinasytnik <alinasytnik1999@gmail.com>
2023-02-08 13:18:21 +0100

    feat: changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -119,7 +119,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
+        Congratulations !!!!!!!!!!!!
         <br />
         <span style={headingAccentStyles}>
           — you just made a Gatsby site! 🎉🎉🎉

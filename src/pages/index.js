741d6c33df1c366086745adfb3bd1141cedd1467
alinasytnik <alinasytnik1999@gmail.com>
2023-03-21 17:32:20 +0100

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -122,7 +122,7 @@ const IndexPage = () => {
        CONGRAAAATS!!!!! 
         <br />
         <span style={headingAccentStyles}>
-          — CONGRAAATS! 🎉🎉🎉
+          — HEY 🎉🎉🎉
         </span>
       </h1>
       <p style={paragraphStyles}>

e7407c6738ed0869a502a0adc2157e2e17239b82
alinasytnik <alinasytnik1999@gmail.com>
2023-08-16 19:16:34 +0200

    feat: GA and GTM added

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,20 @@ module.exports = {
     title: `Gatsby + Vercel`,
     siteUrl: `https://gatsby-template.vercel.app/`,
   },
-  plugins: [],
+  plugins: [ {
+    resolve: "gatsby-plugin-google-tagmanager",
+    options: {
+      id: process.env.GTM_ID,
+      // ... other options ...
+    },
+  },
+  {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: process.env.GA_ID,
+      head: true,
+      anonymize: true,
+    },
+  },],
 };
+

96b356aaab8e8976bfbd4c05c0134410b3071bcd
alinasytnik <alinasytnik1999@gmail.com>
2023-10-20 17:44:15 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -121,10 +121,10 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-      IPNS CHECK2 {process.env.GA_ID}
+      IPNS CHECK!!!! {process.env.GA_ID}
         <br />
         <span style={headingAccentStyles}>
-      IPNS CHECK2
+      IPNS CHECK!!!!!
         </span>
       </h1>
       <p style={paragraphStyles}>
